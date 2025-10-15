aee9bd8e9eba2b4d361d34324651665514b5ac1a
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-15 15:20:30 -0300

    Create Main.java

---
+++ primer_programaONE/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Screen Match");
+        System.out.println("Pelicula: Matrix");
+        
+    }
+}
