046953e225b12d7b586029adbf25db620a9cc36a
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-15 16:09:26 -0300

    Update Decision.java

--- primer_programaONE/src/Decision.java
+++ primer_programaONE/src/Decision.java
@@ -1,3 +1,10 @@
 public class Decision {
 
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 2024;
+        boolean incluidoEnElPlan = true;
+        double notaPelicula = 8.1;
+        double media = (8.1 + 8.6 + 1.0 + notaPelicula) / 3;
+        String tipoPlan = "plus";
+    }
 }
