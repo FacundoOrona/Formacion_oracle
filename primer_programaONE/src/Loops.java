d4c390d26451720b79458a142452b4d71303b040
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-15 16:44:24 -0300

    Update Loops.java

--- primer_programaONE/src/Loops.java
+++ primer_programaONE/src/Loops.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class Loops {
 
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double notaInicial = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Califica la pelicula Matrix: ");
+            double nota = teclado.nextDouble();
+            notaInicial = notaInicial + nota;
+        }
     }
 
 }
