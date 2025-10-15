995506996b8ddded8620a37be7b5cce2f1723522
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-15 15:28:23 -0300

    Update Main.java

--- primer_programaONE/src/Main.java
+++ primer_programaONE/src/Main.java
@@ -3,5 +3,10 @@ public class Main {
         System.out.println("Bienvenido a Screen Match");
         System.out.println("Pelicula: Matrix");
 
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaPelicula = 8.1;
+        double media = (8.1 + 8.6 + 1.0 + notaPelicula) / 3;
+        System.out.println("Media: " + media);
     }
 }
