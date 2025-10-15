d613acbff3920442a6c087735aa3cd2ca4f78651
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-15 16:44:00 -0300

    Update Lectura.java

--- primer_programaONE/src/Lectura.java
+++ primer_programaONE/src/Lectura.java
@@ -2,5 +2,15 @@ import java.util.Scanner;
 
 public class Lectura {
 
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribe el nombre de tu pelicula favorita");
+        String peliculaNombre = teclado.nextLine();
+        System.out.println("Ingrese la fecha de lanzamiento:");
+        int fechaLanzamiento = teclado.nextInt();
+        System.out.println("Ingrese nota: ");
+        double notaPelicula = teclado.nextDouble();
+
+    }
 
 }
