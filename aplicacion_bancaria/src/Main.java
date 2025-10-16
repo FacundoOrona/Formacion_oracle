e8128a1e52136b9327c15fdb6f1a9dcc4c097f51
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-16 18:39:33 -0300

    Update Main.java

--- aplicacion_bancaria/src/Main.java
+++ aplicacion_bancaria/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double saldo = 0;
+        int opcion;
+
+        System.out.println("Nombre del cliente: Facundo");
+        System.out.println("Tipo de cuenta: Corriente");
+        System.out.println("Saldo disponible: $" + saldo);
+
 
     }
 }
