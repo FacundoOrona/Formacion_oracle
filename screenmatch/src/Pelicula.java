e78d95a4d49f5c218cbf4f3d8c32efc922817d57
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-16 19:26:26 -0300

    Create Pelicula.java

---
+++ screenmatch/src/Pelicula.java
@@ -0,0 +1,23 @@
+    String nombre;
+    int fechaLanzamiento;
+    int duracionMinutos;
+    boolean incluidoPlan;
+    double sumaNotas;
+    static double mediaNotas;
+
+
+
+    void muestraPelicula() {
+        System.out.println("Mi pelicula es: " + nombre);
+    }
+
+    void evaluarPelicula(double nota) {
+        sumaNotas += nota;
+        mediaNotas++;
+
+    }
+
+    void promedioNotasPelicula() {
+        System.out.println("Nota promedio final: " + sumaNotas/mediaNotas);
+    }
+
