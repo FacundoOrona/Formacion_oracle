08d9daf3ad58e2d5f7e088859b3d72003360a2e5
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-17 18:09:18 -0300

    Update Principal.java

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,26 +1,30 @@
+import com.facundo.screenmatch.modelos.Pelicula;
+
 public class Principal {
 
     public static void main(String[] args) {
 
         Pelicula pelicula = new Pelicula();
-        pelicula.nombre = "Encanto";
-        pelicula.fechaLanzamiento = 2021;
-        pelicula.duracionMinutos = 120;
-        pelicula.incluidoPlan = true;
+        pelicula.setNombre("Encanto");
+        pelicula.setFechaLanzamiento(2021);
+        pelicula.setDuracionMinutos(120);
+        pelicula.setIncluidoPlan(true);
 
         pelicula.muestraPelicula();
 
         Pelicula pelicula2 = new Pelicula();
-        pelicula2.nombre = "Encanto2";
-        pelicula2.fechaLanzamiento = 2021;
-        pelicula2.duracionMinutos = 120;
-        pelicula2.incluidoPlan = true;
+        pelicula2.setNombre("Encanto2");
+        pelicula2.setFechaLanzamiento(2021);
+        pelicula2.setDuracionMinutos(120);
+        pelicula2.setIncluidoPlan(true);
 
         pelicula2.muestraPelicula();
+
         pelicula2.evaluarPelicula(2.2);
         pelicula2.evaluarPelicula(9.2);
         pelicula2.evaluarPelicula(8.2);
         pelicula2.evaluarPelicula(8.3);
+
         pelicula2.promedioNotasPelicula();
     }
 
