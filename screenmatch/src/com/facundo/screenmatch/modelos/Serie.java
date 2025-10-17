9605b0196d22083e798015ca9740d2d12d3a4645
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-17 18:09:58 -0300

    Update Serie.java

--- screenmatch/src/com/facundo/screenmatch/modelos/Serie.java
+++ screenmatch/src/com/facundo/screenmatch/modelos/Serie.java
@@ -4,5 +4,10 @@ public class Serie extends Titulo {
 
     private int temporadas;
     private int episidosPorTemporada;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
     }
 }
