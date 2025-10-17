31712e9f10eed9745320cd8b3cd5195961b02da8
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-17 18:10:16 -0300

    Update Titulo.java

--- screenmatch/src/com/facundo/screenmatch/modelos/Titulo.java
+++ screenmatch/src/com/facundo/screenmatch/modelos/Titulo.java
@@ -2,4 +2,11 @@ package com.facundo.screenmatch.modelos;
 
 public class Titulo {
 
+    private String nombre;
+    private int fechaLanzamiento;
+    private int duracionMinutos;
+    private boolean incluidoPlan;
+    private double sumaNotas;
+    private static double mediaNotas;
+
 }
