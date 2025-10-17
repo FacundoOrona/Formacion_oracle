352c3b10626a69c37b56fe949376ccdab14ea3d9
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-17 18:09:40 -0300

    Update Pelicula.java

--- screenmatch/src/com/facundo/screenmatch/modelos/Pelicula.java
+++ screenmatch/src/com/facundo/screenmatch/modelos/Pelicula.java
@@ -1,6 +1,11 @@
 package com.facundo.screenmatch.modelos;
+
 public class Pelicula extends Titulo {
 
     private String director;
+
+    public String getDirector() {
+        return director;
+    }
 
 }
