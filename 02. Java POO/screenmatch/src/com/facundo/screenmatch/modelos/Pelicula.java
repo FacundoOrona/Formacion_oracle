01361688e8dfd63c5abfb80a551c91002109c86b
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-20 19:08:55 -0300

    Create Pelicula.java

---
+++ 02. Java POO/screenmatch/src/com/facundo/screenmatch/modelos/Pelicula.java
@@ -0,0 +1,14 @@
+package com.facundo.screenmatch.modelos;
+
+public class Pelicula extends Titulo {
+
+    private String director;
+
+    public String getDirector() {
+        return director;
+    }
+
+    public Pelicula() {
+        this.director = director;
+    }
+}
