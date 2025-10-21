3c0c04a79447772571f67055f0c664034082aa28
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-21 16:02:48 -0300

    Update Episodio.java

--- 02. Java POO/screenmatch/src/com/facundo/screenmatch/modelos/Episodio.java
+++ 02. Java POO/screenmatch/src/com/facundo/screenmatch/modelos/Episodio.java
@@ -1,11 +1,16 @@
 package com.facundo.screenmatch.modelos;
+
 import com.facundo.screenmatch.calculos.Clasificacion;
+
 public class Episodio implements Clasificacion {
     private int numeroEp;
     private String nombre;
     private Serie serie;
     private int totalVisualizaciones;
 
+    public int getNumeroEp() {
+        return numeroEp;
+    }
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
