e718d0ac45f5ea21cdb35c86a6122f2b23aba7d5
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-21 16:02:37 -0300

    Create Episodio.java

---
+++ 02. Java POO/screenmatch/src/com/facundo/screenmatch/modelos/Episodio.java
@@ -0,0 +1,13 @@
+package com.facundo.screenmatch.modelos;
+import com.facundo.screenmatch.calculos.Clasificacion;
+public class Episodio implements Clasificacion {
+    private int numeroEp;
+    private String nombre;
+    private Serie serie;
+    private int totalVisualizaciones;
+
+        else {
+            return 2;
+        }
+    }
+}
