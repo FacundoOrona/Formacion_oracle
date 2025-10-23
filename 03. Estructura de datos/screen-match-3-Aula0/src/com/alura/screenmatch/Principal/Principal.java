b133eb2a97a2da7b0831f2dd7b8f187c7deea7c0
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-23 16:33:09 -0300

    Update Principal.java

--- 03. Estructura de datos/screen-match-3-Aula0/src/com/alura/screenmatch/Principal/Principal.java
+++ 03. Estructura de datos/screen-match-3-Aula0/src/com/alura/screenmatch/Principal/Principal.java
@@ -14,5 +14,47 @@ public class Principal {
         miPelicula.setDuracionEnMinutos(180);
         System.out.println("Duración de la película: " + miPelicula.getDuracionEnMinutos());
 
+
+        Serie lost = new Serie(2000, "Lost" );
+        lost.muestraFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duracion de la série: " + lost.getDuracionEnMinutos());
+
+        Pelicula otraPelicula = new Pelicula(2023, "Avatar");
+        otraPelicula.setDuracionEnMinutos(200);
+
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluido(miPelicula);
+        calculadora.incluido(otraPelicula);
+        calculadora.incluido(lost);
+        System.out.println(calculadora.getTiempoTotal());
+
+        FiltroRecomendacion filtro = new FiltroRecomendacion();
+        filtro.filtra(miPelicula);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizaciones(300);
+        filtro.filtra(episodio);
+
+        var pelicula = new Pelicula(2021, "Señor de los anillos");
+        pelicula.setDuracionEnMinutos(180);
+
+        ArrayList<Pelicula> listaPeliculas = new ArrayList<>();
+        listaPeliculas.add(pelicula);
+        listaPeliculas.add(miPelicula);
+        listaPeliculas.add(otraPelicula);
+
+        System.out.println("Tamaño de la lista: " + listaPeliculas.size());
+        System.out.println("La primer pelicula es: " + listaPeliculas.get(0).getNombre());
+
+        System.out.println(pelicula.toString());
+        System.out.println(listaPeliculas);
+
+
+
     }
 }
