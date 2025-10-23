940e51602083913870b825d24486769db3e09909
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-23 16:33:19 -0300

    Create PrincipalConListas.java

---
+++ 03. Estructura de datos/screen-match-3-Aula0/src/com/alura/screenmatch/Principal/PrincipalConListas.java
@@ -0,0 +1,8 @@
+package com.alura.screenmatch.Principal;
+
+import com.alura.screenmatch.modelos.Pelicula;
+import com.alura.screenmatch.modelos.Serie;
+import com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
