ea7450cd8a88e517bafff1aebb9862ee66b79f2e
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-23 16:33:26 -0300

    Update PrincipalConListas.java

--- 03. Estructura de datos/screen-match-3-Aula0/src/com/alura/screenmatch/Principal/PrincipalConListas.java
+++ 03. Estructura de datos/screen-match-3-Aula0/src/com/alura/screenmatch/Principal/PrincipalConListas.java
@@ -6,3 +6,12 @@ import com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
 
+public class PrincipalConListas {
+    public static void main(String[] args) {
+        Pelicula otraPelicula = new Pelicula(2023, "Avatar");
+        otraPelicula.evalua(9);
+        Pelicula miPelicula = new Pelicula(2021, "Encanto");
+        miPelicula.evalua(8);
+        var pelicula = new Pelicula(2021, "Señor de los anillos");
+        pelicula.evalua(7);
+
