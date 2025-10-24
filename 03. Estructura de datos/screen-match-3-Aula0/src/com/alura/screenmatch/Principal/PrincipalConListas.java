0ff1227dc2aed0e0dde6eaf244a52003e2e26c72
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-24 20:01:05 -0300

    Update PrincipalConListas.java

--- 03. Estructura de datos/screen-match-3-Aula0/src/com/alura/screenmatch/Principal/PrincipalConListas.java
+++ 03. Estructura de datos/screen-match-3-Aula0/src/com/alura/screenmatch/Principal/PrincipalConListas.java
@@ -5,6 +5,9 @@ import com.alura.screenmatch.modelos.Serie;
 import com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalConListas {
     public static void main(String[] args) {
