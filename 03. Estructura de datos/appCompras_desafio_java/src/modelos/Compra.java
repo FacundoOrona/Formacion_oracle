c1d408b359884c86fcd199bd9d624fec5bf02b3f
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-28 20:02:27 -0300

    Update Compra.java

--- 03. Estructura de datos/appCompras_desafio_java/src/modelos/Compra.java
+++ 03. Estructura de datos/appCompras_desafio_java/src/modelos/Compra.java
@@ -1,5 +1,13 @@
 package modelos;
     @Override
+    public String toString() {
+        return "Compra: *" +
+                "descripcion '" + descripcion + '\'' +
+                "/ valor " + valor +
+                '*';
+    }
+
+    @Override
     public int compareTo(Compra otraCompra) {
         return Double.valueOf(this.valor).compareTo(Double.valueOf(otraCompra.getValor()));
     }
