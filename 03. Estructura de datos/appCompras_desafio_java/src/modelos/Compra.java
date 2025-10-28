c71c4a50dd301ceaf298c5ac1d695bf30ee288d8
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-28 20:02:30 -0300

    Update Compra.java

--- 03. Estructura de datos/appCompras_desafio_java/src/modelos/Compra.java
+++ 03. Estructura de datos/appCompras_desafio_java/src/modelos/Compra.java
@@ -1,4 +1,13 @@
 package modelos;
+
+public class Compra implements Comparable<Compra> {
+    private String descripcion;
+    private double valor;
+
+    public Compra(String descripcion, double valor) {
+        this.descripcion = descripcion;
+        this.valor = valor;
+    }
     @Override
     public String toString() {
         return "Compra: *" +
