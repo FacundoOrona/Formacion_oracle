a828d902aa5dc9dece5e03e89195c067ac3b670f
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-30 15:49:25 -0300

    Create PrincipalConBusquedaAPI.java

---
+++ 04. Manejo de APIs/2047-screenmatch4-Aula0/src/com/alura/screenmatch/principal/PrincipalConBusquedaAPI.java
@@ -0,0 +1,13 @@
+package com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalConBusquedaAPI {
+    }
+
+}
