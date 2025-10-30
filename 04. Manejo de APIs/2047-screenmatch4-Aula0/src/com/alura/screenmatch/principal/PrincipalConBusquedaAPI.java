967b09977badb8d5277cc736bf791d055079ddf4
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-30 15:49:32 -0300

    Update PrincipalConBusquedaAPI.java

--- 04. Manejo de APIs/2047-screenmatch4-Aula0/src/com/alura/screenmatch/principal/PrincipalConBusquedaAPI.java
+++ 04. Manejo de APIs/2047-screenmatch4-Aula0/src/com/alura/screenmatch/principal/PrincipalConBusquedaAPI.java
@@ -8,6 +8,13 @@ import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class PrincipalConBusquedaAPI {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escriba el nombre la pelicula que desee buscar");
+        var busqueda = scanner.nextLine();
+
     }
 
 }
