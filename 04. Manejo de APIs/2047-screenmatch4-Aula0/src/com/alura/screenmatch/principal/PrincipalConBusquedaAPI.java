bd954e1eda861f751c2727d1b1c13332090af9f1
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-10-31 17:54:33 -0300

    Update PrincipalConBusquedaAPI.java

--- 04. Manejo de APIs/2047-screenmatch4-Aula0/src/com/alura/screenmatch/principal/PrincipalConBusquedaAPI.java
+++ 04. Manejo de APIs/2047-screenmatch4-Aula0/src/com/alura/screenmatch/principal/PrincipalConBusquedaAPI.java
@@ -1,7 +1,10 @@
 package com.alura.screenmatch.principal;
 
 import com.alura.screenmatch.modelos.Titulo;
+import com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.URI;
@@ -17,6 +20,7 @@ public class PrincipalConBusquedaAPI {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Escriba el nombre la pelicula que desee buscar");
         var busqueda = scanner.nextLine();
+        scanner.close();
 
         String direccion = "https://www.omdbapi.com/?t=" + busqueda + "&apikey=a1de3692";
 
