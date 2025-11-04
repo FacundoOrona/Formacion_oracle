b4f452c41d9f5076ab814d4c28fcabc792fdb35c
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-11-04 15:39:28 -0300

    Update PrincipalConBusquedaAPI.java

--- 04. Manejo de APIs/2047-screenmatch4-Aula0/src/com/alura/screenmatch/principal/PrincipalConBusquedaAPI.java
+++ 04. Manejo de APIs/2047-screenmatch4-Aula0/src/com/alura/screenmatch/principal/PrincipalConBusquedaAPI.java
@@ -24,29 +24,35 @@ public class PrincipalConBusquedaAPI {
 
         String direccion = "https://www.omdbapi.com/?t=" + busqueda + "&apikey=a1de3692";
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(direccion))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        String json = response.body();
-        System.out.println(json);
-
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
-        TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(miTituloOmdb);
-
         try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(direccion))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
+            TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(miTituloOmdb);
+
             Titulo miTitulo = new Titulo(miTituloOmdb);
-            System.out.println(miTitulo);
+            System.out.println("Titulo ya convertido: " + miTitulo);
 
         } catch (NumberFormatException e) {
-            System.out.println("Ocurrio un error: ");
+            System.out.println("Ocurrio un error: NumberFormatException");
             System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ocurrio un error: IllegalArgumentException");
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Ocurrio un erro inesperado");
+            System.out.println(e.getMessage() );
         }
 
         System.out.println("Finalizo la ejecucion");
