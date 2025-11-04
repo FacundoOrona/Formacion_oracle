183bb95d86f9a2a0f13c3af5dd0a31d802c1e95d
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-11-04 16:09:48 -0300

    Update PrincipalConBusquedaAPI.java

--- 04. Manejo de APIs/2047-screenmatch4-Aula0/src/com/alura/screenmatch/principal/PrincipalConBusquedaAPI.java
+++ 04. Manejo de APIs/2047-screenmatch4-Aula0/src/com/alura/screenmatch/principal/PrincipalConBusquedaAPI.java
@@ -7,6 +7,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -46,6 +47,10 @@ public class PrincipalConBusquedaAPI {
 
             Titulo miTitulo = new Titulo(miTituloOmdb);
             System.out.println("Titulo ya convertido: " + miTitulo);
+
+            FileWriter escritura = new FileWriter("Peliculas.txt");
+            escritura.write(miTitulo.toString());
+            escritura.close();
 
         } catch (NumberFormatException e) {
             System.out.println("Ocurrio un error: NumberFormatException");
