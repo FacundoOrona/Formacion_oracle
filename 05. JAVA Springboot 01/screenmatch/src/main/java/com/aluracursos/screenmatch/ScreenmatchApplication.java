c6d80ced7b9b6974eb85cce062fbcbd55c366b38
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-11-21 21:08:56 -0300

    Update ScreenmatchApplication.java

--- 05. JAVA Springboot 01/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ 05. JAVA Springboot 01/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,9 +1,6 @@
 package com.aluracursos.screenmatch;
 
-import com.aluracursos.screenmatch.model.DatosEpisodio;
-import com.aluracursos.screenmatch.model.DatosSerie;
-import com.aluracursos.screenmatch.service.ConsumoAPIService;
-import com.aluracursos.screenmatch.service.ConvierteDatosService;
+import com.aluracursos.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,20 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        var consumoAPIService = new ConsumoAPIService();
-        var jsonSerie = consumoAPIService.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&apikey=a1de3692");
-        var jsonEpisodio = consumoAPIService.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&season=1&episode=1&apikey=a1de3692");
-
-        System.out.println("**** JSON SIN CONVERTIR ****");
-        System.out.println(jsonSerie);
-        System.out.println(jsonEpisodio);
-
-        System.out.println("**** JSON CON CONVERSION ****");
-        ConvierteDatosService conversor = new ConvierteDatosService();
-        var datosSerie = conversor.obtenerDatos(jsonSerie, DatosSerie.class);
-        System.out.println(datosSerie);
-
-        var datosEpisodio = conversor.obtenerDatos(jsonEpisodio, DatosEpisodio.class);
-        System.out.println(datosEpisodio);
+        Principal principal = new Principal();
+        principal.mostrarMenu();
     }
 }
