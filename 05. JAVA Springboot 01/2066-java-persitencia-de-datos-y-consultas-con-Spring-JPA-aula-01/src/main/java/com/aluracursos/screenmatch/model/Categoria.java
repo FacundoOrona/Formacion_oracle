dbb14ae15515f41ccc1420cfbb8a828e444d7f70
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-12-12 20:29:48 -0300

    Update Categoria.java

--- 05. JAVA Springboot 01/2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-aula-01/src/main/java/com/aluracursos/screenmatch/model/Categoria.java
+++ 05. JAVA Springboot 01/2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-aula-01/src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -1,20 +1,31 @@
 package com.aluracursos.screenmatch.model;
 
 public enum Categoria {
-    ACCION("Action"),
-    ROMANCE("Romance"),
-    COMEDIA("Comedy"),
-    DRAMA("Drama"),
-    CRIMEN("Crime");
+    ACCION("Action", "Accion"),
+    ROMANCE("Romance", "Romance"),
+    COMEDIA("Comedy", "Comedia"),
+    DRAMA("Drama", "Drama"),
+    CRIMEN("Crime", "Crimen");
 
     private String categoriaOmdb;
-    Categoria (String categoriaOmdb){
+    private String categoriaEsp;
+    Categoria (String categoriaOmdb, String categoriaEsp){
         this.categoriaOmdb = categoriaOmdb;
+        this.categoriaEsp = categoriaEsp;
     }
 
     public static Categoria fromString(String text) {
         for (Categoria categoria : Categoria.values()) {
             if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada: " + text);
+    }
+
+    public static Categoria fromEsp(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaEsp.equalsIgnoreCase(text)) {
                 return categoria;
             }
         }
