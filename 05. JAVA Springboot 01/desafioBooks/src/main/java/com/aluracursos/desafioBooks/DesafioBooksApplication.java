0ce36c1c68d18a0b3271b5efd7080f9777ec7037
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-12-12 17:27:42 -0300

    Update DesafioBooksApplication.java

--- 05. JAVA Springboot 01/desafioBooks/src/main/java/com/aluracursos/desafioBooks/DesafioBooksApplication.java
+++ 05. JAVA Springboot 01/desafioBooks/src/main/java/com/aluracursos/desafioBooks/DesafioBooksApplication.java
@@ -1,13 +1,21 @@
 package com.aluracursos.desafioBooks;
 
+import com.aluracursos.desafioBooks.Principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioBooksApplication {
+public class DesafioBooksApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioBooksApplication.class, args);
 	}
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal();
+        principal.muestraMenu();
+    }
 }
