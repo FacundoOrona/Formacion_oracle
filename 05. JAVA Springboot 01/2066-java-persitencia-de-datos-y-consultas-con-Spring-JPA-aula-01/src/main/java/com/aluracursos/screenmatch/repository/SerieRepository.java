a5be7dc2a7adc4482d14c3f121eeae467ff4ced9
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-12-15 19:13:59 -0300

    Update SerieRepository.java

--- 05. JAVA Springboot 01/2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-aula-01/src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++ 05. JAVA Springboot 01/2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-aula-01/src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -3,6 +3,7 @@ package com.aluracursos.screenmatch.repository;
 import com.aluracursos.screenmatch.model.Categoria;
 import com.aluracursos.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +14,8 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     List<Serie> findTop5ByOrderByEvaluacionDesc();
 
     List<Serie> findByGenero(Categoria categoria);
+
+    //Los : indican que el atributo es de los recibidos por parametros
+    @Query(value = "SELECT s FROM Serie s WHERE s.totalTemporadas <= :totalTemporadas AND s.evaluacion >= :evaluacion")
+    List<Serie> seriesPorTemporadaYEvaluacion(int totalTemporadas, Double evaluacion);
 }
