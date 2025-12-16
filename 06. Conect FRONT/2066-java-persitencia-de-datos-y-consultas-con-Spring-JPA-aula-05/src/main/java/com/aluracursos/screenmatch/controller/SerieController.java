8ad4027f2f5b32a18f4295f9a106e82ad56c81a9
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-12-16 17:57:43 -0300

    Update SerieController.java

--- 06. Conect FRONT/2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-aula-05/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ 06. Conect FRONT/2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-aula-05/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,14 +1,41 @@
 package com.aluracursos.screenmatch.controller;
 
+import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.service.SerieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
-    @GetMapping("/series")
-    public String mostrarMensaje() {
-        return "Mensaje aleatorio";
+    @Autowired
+    private SerieService service;
+
+    @GetMapping
+    public List<SerieDTO> obtenerTodasLasSeries() {
+        return service.obtenerSeries();
     }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obtenerTop5() {
+        return service.obtenerTop5();
+    }
+
+    @GetMapping("/lanzamientos")
+    public List<SerieDTO> obtenerRecientes () {
+        return service.obtenerLanzamientosMasRecientes();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obtenerPorId(@PathVariable Long id) {
+        return service.obtenerPorId(id);
+    }
+
 }
