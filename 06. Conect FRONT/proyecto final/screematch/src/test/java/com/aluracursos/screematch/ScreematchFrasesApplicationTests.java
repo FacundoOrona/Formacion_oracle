a1a9b5b16b1001edb1a21c27d71bcefd7ccae1b4
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-12-18 16:18:08 -0300

    create project backend

---
+++ 06. Conect FRONT/proyecto final/screematch/src/test/java/com/aluracursos/screematch/ScreematchFrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screematch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreematchFrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
