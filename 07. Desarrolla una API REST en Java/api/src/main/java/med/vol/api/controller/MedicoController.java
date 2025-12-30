aa39463daa95c7b7cc6bd8fe00598b44d1991536
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-12-30 16:27:29 -0300

    Add validation and transaction to Medico registration
    Added @Valid annotation to validate incoming DatosRegistroMedicoDTO and @Transactional to ensure atomicity of the registration process in MedicoController.

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/controller/MedicoController.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/controller/MedicoController.java
@@ -1,9 +1,11 @@
 package med.vol.api.controller;
 
+import jakarta.validation.Valid;
 import med.vol.api.dto.DatosRegistroMedicoDTO;
 import med.vol.api.model.Medico;
 import med.vol.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,8 +15,9 @@ public class MedicoController {
     @Autowired
     MedicoRepository medicoRepository;
 
+    @Transactional
     @PostMapping
-    public void registar(@RequestBody DatosRegistroMedicoDTO datos) {
+    public void registar(@RequestBody @Valid DatosRegistroMedicoDTO datos) {
         medicoRepository.save(new Medico(datos));
     }
 
