e9861e045e53f8773dec342ff94e9534d5a87317
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-12-30 15:27:21 -0300

    Add Direccion model class
    Introduces the Direccion class with fields for address details and Lombok annotations for boilerplate code reduction.

---
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/model/Direccion.java
@@ -0,0 +1,20 @@
+package med.vol.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Direccion {
+    private String calle;
+    private String numero;
+    private String complemento;
+    private String barrio;
+    private String codigo_postal;
+    private String ciudad;
+    private String estado;
+}
