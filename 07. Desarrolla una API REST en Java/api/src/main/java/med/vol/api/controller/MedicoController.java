8527843f7e2e4b0733d3b19853dcf472dd2ac25c
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-12-30 15:54:36 -0300

    Add MedicoRepository to MedicoController for saving data
    Injected MedicoRepository into MedicoController and updated the registrar method to save new Medico instances using the repository. This enables persistence of Medico data received via POST requests.

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/controller/MedicoController.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/controller/MedicoController.java
@@ -1,15 +1,21 @@
 package med.vol.api.controller;
 
 import med.vol.api.dto.DatosRegistroMedicoDTO;
+import med.vol.api.model.Medico;
+import med.vol.api.repository.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    MedicoRepository medicoRepository;
+
     @PostMapping
     public void registar(@RequestBody DatosRegistroMedicoDTO datos) {
-
+        medicoRepository.save(new Medico(datos));
     }
 
 
