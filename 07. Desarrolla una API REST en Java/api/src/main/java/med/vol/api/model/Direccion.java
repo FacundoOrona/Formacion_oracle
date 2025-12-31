56e5816e561096be61f1f1f3a97ecff93fc62b79
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-12-31 14:55:22 -0300

    Add method to update Direccion fields selectively
    Introduces the actualizarDireccion method to the Direccion class, allowing selective updates of address fields only if new values are provided. This improves flexibility when updating address information.

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/model/Direccion.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/model/Direccion.java
@@ -28,4 +28,28 @@ public class Direccion {
         this.codigo_postal = direccion.codigo_postal();
         this.estado = direccion.estado();
     }
+
+    public void actualizarDireccion(DatosDireccion direccion) {
+        if (direccion.calle() != null) {
+            this.calle = direccion.calle();
+        }
+        if (direccion.numero() != null) {
+            this.numero = direccion.numero();
+        }
+        if (direccion.complemento() != null) {
+            this.complemento = direccion.complemento();
+        }
+        if (direccion.barrio() != null) {
+            this.barrio = direccion.barrio();
+        }
+        if (direccion.ciudad() != null) {
+            this.ciudad = direccion.ciudad();
+        }
+        if (direccion.codigo_postal() != null) {
+            this.codigo_postal = direccion.codigo_postal();
+        }
+        if (direccion.estado() != null) {
+            this.estado = direccion.estado();
+        }
+    }
 }
