05ecaf44b90e9e2443a8db1613036c4b6b27e1ed
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-12-31 14:39:45 -0300

    Add paginated GET endpoint for listing medicos
    Introduces a GET endpoint to /medicos that returns a paginated list of medicos using DatosListaMedicoDTO. Supports pagination and sorting via Pageable, improving API usability for large datasets.

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/controller/MedicoController.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/controller/MedicoController.java
@@ -1,12 +1,18 @@
 package med.vol.api.controller;
 
 import jakarta.validation.Valid;
+import med.vol.api.dto.DatosListaMedicoDTO;
 import med.vol.api.dto.DatosRegistroMedicoDTO;
 import med.vol.api.model.Medico;
 import med.vol.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,6 +25,14 @@ public class MedicoController {
     @PostMapping
     public void registar(@RequestBody @Valid DatosRegistroMedicoDTO datos) {
         medicoRepository.save(new Medico(datos));
+    }
+
+    /// medicos?size=10&page=1
+    @GetMapping
+    public Page<DatosListaMedicoDTO> listar(@PageableDefault(size = 10, page = 0,
+                                                sort = {"nombre"})
+                                                Pageable paginacion) {
+        return medicoRepository.findAll(paginacion).map(DatosListaMedicoDTO::new);
     }
 
 
