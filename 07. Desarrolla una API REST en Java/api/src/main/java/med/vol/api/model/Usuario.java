85d73be65c1224939dcf0707cfbd71231bc667e8
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-08 23:48:25 -0300

    Update Usuario.java

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/model/Usuario.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/model/Usuario.java
@@ -16,3 +16,12 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Size(max = 100, message = "El login no puede superar 100 caracteres")
+    @Column(name = "login", length = 100, nullable = false, unique = true)
+    private String login;
+
+    @Size(max = 255, message = "La contraseña no puede superar 255 caracteres")
+    @Column(name = "contrasena", length = 255, nullable = false)
+    private String contrasena;
+}
