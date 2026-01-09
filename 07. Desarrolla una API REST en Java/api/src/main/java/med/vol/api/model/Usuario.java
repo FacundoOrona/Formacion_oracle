6977edc2572c54865980dcc7534302c9a081bd56
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-09 00:43:45 -0300

    Update Usuario.java

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/model/Usuario.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/model/Usuario.java
@@ -3,6 +3,13 @@ package med.vol.api.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,7 +18,7 @@ import lombok.*;
 @EqualsAndHashCode(of = "id")
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +31,39 @@ public class Usuario {
     @Size(max = 255, message = "La contraseña no puede superar 255 caracteres")
     @Column(name = "contrasena", length = 255, nullable = false)
     private String contrasena;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return contrasena;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
