4f9c7e2be29cefe0325d2fd21e6ea6747d757be3
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-09 00:00:05 -0300

    Update AutenticacionService.java

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/service/AutenticacionService.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/service/AutenticacionService.java
@@ -1,6 +1,8 @@
 package med.vol.api.service;
 
+import med.vol.api.repository.UsuarioRepository;
 import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,8 +11,16 @@ import org.springframework.stereotype.Service;
 @Service
 
 public class AutenticacionService implements UserDetailsService {
+
+
+    private final UsuarioRepository usuarioRepository;
+
+    public AutenticacionService (UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return usuarioRepository.findByLogin(username);
     }
 }
