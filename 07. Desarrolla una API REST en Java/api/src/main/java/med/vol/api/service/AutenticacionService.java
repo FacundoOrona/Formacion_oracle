f9e22a8e31304334247e2aeab1ee8198ed2de9a1
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-08 23:53:52 -0300

    Implement UserDetailsService in AutenticacionService
    AutenticacionService now implements UserDetailsService and provides a stub for loadUserByUsername. This prepares the service for integration with Spring Security authentication mechanisms.

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/service/AutenticacionService.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/service/AutenticacionService.java
@@ -1,8 +1,16 @@
 package med.vol.api.service;
 
+import org.apache.catalina.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 
-public class AutenticacionService {
+public class AutenticacionService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
