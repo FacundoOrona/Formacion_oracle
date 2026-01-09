274910369752c99e6d88bdcc9459ee36e016c991
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-09 00:00:02 -0300

    Add findByLogin method to UsuarioRepository
    Introduces a new method to retrieve UserDetails by login in UsuarioRepository, enabling integration with Spring Security authentication mechanisms.

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/repository/UsuarioRepository.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/repository/UsuarioRepository.java
@@ -2,6 +2,8 @@ package med.vol.api.repository;
 
 import med.vol.api.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String username);
 }
