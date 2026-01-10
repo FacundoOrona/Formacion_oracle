d274bcf11a3cb33ca93d052f64d63112a59b0081
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-09 21:51:06 -0300

    Use secret key from properties for JWT token
    Replaces hardcoded JWT secret with value from application properties using @Value. Also adds user ID as a claim in the generated token for improved identification.

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/infra/security/TokenService.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.vol.api.infra.security;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import med.vol.api.model.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 
@@ -14,13 +15,17 @@ import java.util.Date;
 @Service
 public class TokenService {
 
+    @Value("${api.security.token.secret}")
+    private String secretKey;
+
     public String generarToken(Usuario usuario) {
         try {
-            var algoritmo = Algorithm.HMAC256("123456");
+            var algoritmo = Algorithm.HMAC256(secretKey);
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
                     .withExpiresAt(fechaExpiracion())
+                    .withClaim("id", usuario.getId())
                     .sign(algoritmo);
         } catch (JWTCreationException exception){
             // Invalid Signing configuration / Couldn't convert Claims.
