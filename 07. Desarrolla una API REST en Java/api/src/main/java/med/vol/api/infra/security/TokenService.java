4dbd35642be38da47dc4bea8123f0f8089c9de4b
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-09 21:33:10 -0300

    Update TokenService.java

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/infra/security/TokenService.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/infra/security/TokenService.java
@@ -2,21 +2,33 @@ package med.vol.api.infra.security;
 
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import med.vol.api.model.Usuario;
 import org.springframework.stereotype.Service;
+import com.auth0.jwt.JWT;
 
-import static org.springframework.security.config.Elements.JWT;
+import java.time.Instant;
+import java.util.Date;
 
 @Service
 public class TokenService {
 
-    public String generarToken() {
+    public String generarToken(Usuario usuario) {
         try {
-            var algoritmo = Algorithm.RSA256(rsaPublicKey, rsaPrivateKey);
-            String token = JWT.create()
-                    .withIssuer("auth0")
-                    .sign(algorithm);
+            var algoritmo = Algorithm.HMAC256("123456");
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(fechaExpiracion())
+                    .sign(algoritmo);
         } catch (JWTCreationException exception){
             // Invalid Signing configuration / Couldn't convert Claims.
+            throw new RuntimeException("Error al general el token JWT");
         }
     }
+
+    private Instant fechaExpiracion() {
+        
+    }
+
+
 }
