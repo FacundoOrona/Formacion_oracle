009aeac0b3015516816964ee44835f7bc08433f9
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-12 22:52:03 -0300

    Update SecurityFilter.java

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/infra/security/SecurityFilter.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/infra/security/SecurityFilter.java
@@ -12,3 +12,9 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = obtenerToken(request);
+
+        filterChain.doFilter(request, response);
+    }
