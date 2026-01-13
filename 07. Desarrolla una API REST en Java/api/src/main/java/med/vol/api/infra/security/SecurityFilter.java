70c39ac929f6756d5829f9c8a831eafdb506843a
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-12 22:51:59 -0300

    Update SecurityFilter.java

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/infra/security/SecurityFilter.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/infra/security/SecurityFilter.java
@@ -6,3 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
