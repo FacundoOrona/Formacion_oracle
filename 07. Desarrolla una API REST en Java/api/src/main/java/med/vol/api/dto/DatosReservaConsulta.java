0fb496e8f43c30bb66bbff0fea7d13af9d10d0b1
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-15 16:11:01 -0300

    Create DatosReservaConsulta.java

---
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/dto/DatosReservaConsulta.java
@@ -0,0 +1,16 @@
+package med.vol.api.dto;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record DatosReservaConsulta(
+        Long idMedico,
+        @NotNull
+        Long idPaciente,
+        @NotNull
+        @Future
+        LocalDateTime fecha
+) {
+}
