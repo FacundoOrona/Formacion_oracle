93dca7082e3948ca36af2331c335f453e5b35aa8
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-15 17:20:32 -0300

    Update ConsultaController.java

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/controller/ConsultaController.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/controller/ConsultaController.java
@@ -3,6 +3,7 @@ package med.vol.api.controller;
 import jakarta.validation.Valid;
 import med.vol.api.dto.DatosDetalleConsulta;
 import med.vol.api.dto.DatosReservaConsulta;
+import med.vol.api.service.ConsultasService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/consultas")
 public class ConsultaController {
 
+    private final ConsultasService service;
+
+    public ConsultaController(ConsultasService consultasService) {
+        this.service = consultasService;
+    }
+
     @PostMapping
     @Transactional
     public ResponseEntity reservar(@RequestBody @Valid DatosReservaConsulta datos) {
-        System.out.println(datos);
+        service.reservar(datos);
         return ResponseEntity.ok(new DatosDetalleConsulta(null, null, null, null));
     }
 }
