df7d34aa2ba491bd146d5834eb56cd807801f78a
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-15 16:10:56 -0300

    Create DatosRegistroMedico.java

---
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/dto/DatosRegistroMedico.java
@@ -0,0 +1,23 @@
+package med.vol.api.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.vol.api.model.Especialidad;
+
+public record DatosRegistroMedico(
+        @NotBlank
+        String nombre,
+        @NotBlank @Email
+        String email,
+        @NotBlank String telefono,
+        @NotBlank @Pattern(regexp = "\\d{7,9}")
+        String documento,
+        @NotNull
+        Especialidad especialidad,
+        @NotNull @Valid
+        DatosDireccion direccion
+) {
+}
