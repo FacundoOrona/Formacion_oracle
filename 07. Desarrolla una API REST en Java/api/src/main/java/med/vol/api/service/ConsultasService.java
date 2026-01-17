d11cd71cb9a82f083d6284e3bc2f4d9d26e8acbc
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-17 01:33:40 -0300

    Update ConsultasService.java

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/service/ConsultasService.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/service/ConsultasService.java
@@ -1,6 +1,7 @@
 package med.vol.api.service;
 
 import jakarta.persistence.EntityNotFoundException;
+import med.vol.api.dto.DatosDetalleConsulta;
 import med.vol.api.dto.DatosReservaConsulta;
 import med.vol.api.model.Consulta;
 import med.vol.api.model.Especialidad;
@@ -9,7 +10,10 @@ import med.vol.api.model.Paciente;
 import med.vol.api.repository.ConsultaRepository;
 import med.vol.api.repository.MedicoRepository;
 import med.vol.api.repository.PacienteRepository;
+import med.vol.api.validaciones.ValidadorDeConsultas;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ConsultasService {
@@ -17,22 +21,30 @@ public class ConsultasService {
     private final ConsultaRepository consultaRepository;
     private final MedicoRepository medicoRepository;
     private final PacienteRepository pacienteRepository;
+    private final List<ValidadorDeConsultas> validadores;
 
-    public ConsultasService(ConsultaRepository consultaRepository, MedicoRepository medicoRepository, PacienteRepository pacienteRepository) {
+    public ConsultasService(ConsultaRepository consultaRepository, MedicoRepository medicoRepository, PacienteRepository pacienteRepository, List<ValidadorDeConsultas> validadorDeConsultas) {
         this.consultaRepository = consultaRepository;
         this.medicoRepository = medicoRepository;
         this.pacienteRepository = pacienteRepository;
+        this.validadores = validadorDeConsultas;
     }
 
-    public void reservar (DatosReservaConsulta datos) {
+    public DatosDetalleConsulta reservar (DatosReservaConsulta datos) {
+
+        //Hacemos un for each por la lista de clases que implementan la interfaz de validaciones,
+        //que todos unan la funcion validar()
+        validadores.forEach(v -> v.validar(datos));
+
         Medico medico = medicoRepository.findById(datos.idMedico())
                 .orElseThrow(()-> new EntityNotFoundException("Medico no encontrado con ID: "+datos.idMedico()));
         Paciente paciente = pacienteRepository.findById(datos.idPaciente())
                 .orElseThrow(() -> new EntityNotFoundException("Paciente no encontrado con ID: "+datos.idPaciente()));
-        Especialidad especialidad = datos.especialidad();
 
-        var consulta = new Consulta(null, medico, paciente, datos.fecha(), especialidad);
+        var consulta = new Consulta(null, medico, paciente, datos.fecha());
 
         consultaRepository.save(consulta);
+
+        return new DatosDetalleConsulta(consulta);
     }
 }
