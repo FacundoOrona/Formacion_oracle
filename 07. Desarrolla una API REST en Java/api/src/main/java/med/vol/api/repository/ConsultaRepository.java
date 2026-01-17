69ded443ff5c4d0dd24bbec4f0b76efd7bdf167c
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-01-17 01:33:36 -0300

    Update ConsultaRepository.java

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/repository/ConsultaRepository.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/repository/ConsultaRepository.java
@@ -1,8 +1,17 @@
 package med.vol.api.repository;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import med.vol.api.model.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ConsultaRepository extends JpaRepository<Consulta,Long> {
 
+    boolean existsByPacienteIdAndFechaBetween(Long idPaciente,
+                                              LocalDateTime primerHorario,
+                                              LocalDateTime ultimoHorario);
+
+    boolean existsByMedicoIdAndFecha(Long idMedico,LocalDateTime fecha);
 }
