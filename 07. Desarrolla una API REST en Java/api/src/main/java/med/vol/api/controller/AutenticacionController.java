5bb3635ff2a5c106302565640ac5760adf04ec11
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-03-18 18:16:09 -0300

    Update AutenticacionController.java

--- 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/controller/AutenticacionController.java
+++ 07. Desarrolla una API REST en Java/api/src/main/java/med/vol/api/controller/AutenticacionController.java
@@ -2,16 +2,22 @@ package med.vol.api.controller;
 
 import jakarta.validation.Valid;
 import med.vol.api.dto.DatosAutenticacion;
+import med.vol.api.dto.DatosListadoPaciente;
+import med.vol.api.dto.DatosRegistroPaciente;
 import med.vol.api.dto.DatosTokenJWT;
 import med.vol.api.infra.security.TokenService;
+import med.vol.api.model.Paciente;
 import med.vol.api.model.Usuario;
+import med.vol.api.repository.PacienteRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/auth")
@@ -19,11 +25,14 @@ public class AutenticacionController {
 
     private final TokenService tokenService;
     private final AuthenticationManager manager;
+    private final PacienteRepository pacienteRepository;
 
     public AutenticacionController(AuthenticationManager manager,
-                                   TokenService tokenService) {
+                                   TokenService tokenService,
+                                   PacienteRepository pacienteRepository) {
         this.manager = manager;
         this.tokenService = tokenService;
+        this.pacienteRepository = pacienteRepository;
     }
 
     @PostMapping("/login")
@@ -35,5 +44,16 @@ public class AutenticacionController {
         var tokenJWT = tokenService.generarToken((Usuario) authentication.getPrincipal());
 
         return ResponseEntity.ok(new DatosTokenJWT(tokenJWT));
+    }
+
+    @PostMapping("/pacientes")
+    @Transactional
+    public ResponseEntity registrarPaciente(@Valid @RequestBody DatosRegistroPaciente datos,
+                                            UriComponentsBuilder uriComponentsBuilder) {
+        var paciente = new Paciente(datos);
+        pacienteRepository.save(paciente);
+        var uri = uriComponentsBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DatosListadoPaciente(paciente));
     }
 }
